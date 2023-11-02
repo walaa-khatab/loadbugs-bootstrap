4f5f3b2d924db2086a301ecc3561d207e11bca98
yasmin <yasmintolba227@gmail.com>
2023-11-02 23:02:06 +0200

    product details and dashboard page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
     /*----feather icons init-----*/
-        feather.replace();
+    feather.replace();
 
     /*------datarangepicker init----*/
     $('input[name="daterange"]').daterangepicker();
 
-    
+
 });
+
+// owl carousel
+(function ($) {
+
+
+    var carousel = function () {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            // autoplay: true,
+            // margin: 30,
+            // animateOut: 'fadeOut',
+            // animateIn: 'fadeIn',
+            nav: true,
+            dots: true,
+            // autoplayHoverPause: false,
+            items: 1,
+            navText: ["<p><small> <span>&#8592;</span> Back</small><span class='ion-ios-arrow-round-back'></span></p>", "<p><small>Next <span>&#8594;</span></small><span class='ion-ios-arrow-round-forward'></span></p>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        });
+
+    };
+    carousel();
+
+})(jQuery);
